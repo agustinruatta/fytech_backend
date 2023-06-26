251639e43291a21968697d9347a77448d31efc7a
Agustín Ruatta <agustinruatta@gmail.com>
2023-06-25 21:40:12 -0300

    Get UVA data

--- src/MarketData/MarketDataModule.ts
+++ src/MarketData/MarketDataModule.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MarketDataService } from './MarketDataService';
 import { MarketDataController } from './MarketDataController';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   controllers: [MarketDataController],
   providers: [MarketDataService],
+  imports: [ConfigModule.forRoot()],
 })
 export class MarketDataModule {}
--- src/MarketData/MarketDataService.ts
+++ src/MarketData/MarketDataService.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { GetCurrentMarketDataResponse } from './response/GetCurrentMarketDataResponse';
+import axios from 'axios';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MarketDataService {
-  getCurrentMarketData(code: string) {
-    return new GetCurrentMarketDataResponse(100, new Date());
+  private configService: ConfigService;
+
+  constructor(configService: ConfigService) {
+    this.configService = configService;
+  }
+
+  async getCurrentMarketData(
+    code: string,
+  ): Promise<GetCurrentMarketDataResponse> {
+    if (code !== 'uva_ar') {
+      throw new Error('Invalid code');
+    }
+
+    const response: { d: string; v: number }[] = (
+      await axios.get('https://api.estadisticasbcra.com/uva', {
+        headers: {
+          Authorization:
+            'Bearer ' + this.configService.get('ESTADISTICAS_BCRA_API_KEY'),
+        },
+      })
+    ).data;
+
+    const lastData = response[response.length - 1];
+
+    return new GetCurrentMarketDataResponse(lastData.v, new Date(lastData.d));
   }
 }
