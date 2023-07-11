efb7ec298822846e4f43e3f22ae768d8793bbbd7
Agustín Ruatta <agustinruatta@gmail.com>
2023-07-11 17:16:16 -0300

    Added swagger integration

--- src/MarketData/MarketDataController.ts
+++ src/MarketData/MarketDataController.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { MarketDataService } from './MarketDataService';
+import { GetCurrentMarketDataResponse } from './response/GetCurrentMarketDataResponse';
 
 @Controller('market-data')
 export class MarketDataController {
   constructor(private readonly marketDataService: MarketDataService) {}
 
   @Get('/current/:code')
-  findOne(@Param('code') code: string) {
+  findOne(@Param('code') code: string): Promise<GetCurrentMarketDataResponse> {
     return this.marketDataService.getCurrentMarketData(code);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService: ConfigService = app.get<ConfigService>(ConfigService);
+
+
+  if (configService.get('CREATE_SWAGGER_DOC') === 'true') {
+    const config = new DocumentBuilder()
+      .setTitle('Fytech backend')
+      .setDescription('Fytech backend API description')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+  }
+
   await app.listen(3000);
 }
 bootstrap();
