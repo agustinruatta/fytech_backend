7b95a4dd2d57d24d63be611b30cdc91aa960bcf5
Agustín Ruatta <agustinruatta@gmail.com>
2023-07-16 16:53:54 -0300

    Fixed lint

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ConfigService } from "@nestjs/config";
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService: ConfigService = app.get<ConfigService>(ConfigService);
-
 
   if (configService.get('CREATE_SWAGGER_DOC') === 'true') {
     const config = new DocumentBuilder()
