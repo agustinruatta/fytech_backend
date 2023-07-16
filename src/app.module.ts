79454b3fd0640fdc7567a5ba2a1e00dc23d0da3c
Agustín Ruatta <agustinruatta@gmail.com>
2023-07-16 16:43:25 -0300

    Created Shared module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MarketDataModule } from './MarketData/MarketDataModule';
+import { SharedModule } from './Shared/SharedModule';
 
 @Module({
-  imports: [MarketDataModule],
+  imports: [MarketDataModule, SharedModule],
   controllers: [AppController],
 })
 export class AppModule {}
