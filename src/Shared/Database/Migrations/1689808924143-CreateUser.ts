f4fd67836b7582da9127c995e39957d3aee68565
Agustín Ruatta <agustinruatta@gmail.com>
2023-07-30 18:07:52 -0300

    Enable uuid extension

--- src/Shared/Database/Migrations/1689808924143-CreateUser.ts
+++ src/Shared/Database/Migrations/1689808924143-CreateUser.ts
@@ -2,6 +2,8 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateUser1689808924143 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";');
+
     await queryRunner.query(
       `CREATE TABLE users
        (
