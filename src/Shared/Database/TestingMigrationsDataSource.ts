5554dae09a492cc6970e5654c2db1e25453c6524
Agustín Ruatta <agustinruatta@gmail.com>
2023-09-15 23:03:52 -0300

    Fix

--- src/Shared/Database/TestingMigrationsDataSource.ts
+++ src/Shared/Database/TestingMigrationsDataSource.ts
@@ -1,8 +1,7 @@
 import dataSource from './MigrationsDataSource';
 import { DataSource } from 'typeorm';
 
-const databaseUrl =
-  'postgresql://fytech_test:test@postgres_test:5433/fytech_test';
+const databaseUrl = 'postgresql://fytech_test:test@localhost:5433/fytech_test';
 
 export default new DataSource({
   type: 'postgres',
