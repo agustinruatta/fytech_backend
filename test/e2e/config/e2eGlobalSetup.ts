d5b32ce621672ab34b38f202e739f77311f3dd59
Agustín Ruatta <agustinruatta@gmail.com>
2023-09-17 18:28:41 -0300

    Debugging

--- test/e2e/config/e2eGlobalSetup.ts
+++ test/e2e/config/e2eGlobalSetup.ts
@@ -1,6 +1,10 @@
 import * as nock from 'nock';
 
+console.log('OUT GLOBAL SETUP');
+
 module.exports = async function () {
+  console.log('IN GLOBAL SETUP');
+  console.log(process.env.GITHUB_ACTIONS);
   if (!process.env.GITHUB_ACTIONS) {
     nock.disableNetConnect();
     nock.enableNetConnect(/127\.0\.0\.1/);
--- test/e2e/config/setup-e2e-jest.ts
+++ test/e2e/config/setup-e2e-jest.ts
@@ -1,7 +1,11 @@
+console.log('E2" setup');
+
 beforeEach(async () => {
+  console.log('BEFORE EACH');
   await global.testingQueryRunner.startTransaction();
 });
 
 afterEach(async () => {
+  console.log('AFTER EACH');
   await global.testingQueryRunner.rollbackTransaction();
 });
