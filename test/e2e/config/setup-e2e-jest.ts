7e5d33f4c051c13f726ec9b392069bc78d482b40
Agustín Ruatta <agustinruatta@gmail.com>
2023-09-17 18:30:32 -0300

    Revert "Debugging"
    This reverts commit d5b32ce621672ab34b38f202e739f77311f3dd59.

--- test/e2e/config/setup-e2e-jest.ts
+++ test/e2e/config/setup-e2e-jest.ts
@@ -1,11 +1,7 @@
-console.log('E2" setup');
-
 beforeEach(async () => {
-  console.log('BEFORE EACH');
   await global.testingQueryRunner.startTransaction();
 });
 
 afterEach(async () => {
-  console.log('AFTER EACH');
   await global.testingQueryRunner.rollbackTransaction();
 });
