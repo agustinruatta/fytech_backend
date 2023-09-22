a6fee71feabb9d95c85856a549a52d8f9b1a449e
Agustín Ruatta <agustinruatta@gmail.com>
2023-09-21 23:18:41 -0300

    Fixed bug

--- src/Auth/AuthModule.ts
+++ src/Auth/AuthModule.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './AuthController';
 import { AuthService } from './AuthService';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../Users/Entities/User';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([User])],
 })
 export class AuthModule {}
